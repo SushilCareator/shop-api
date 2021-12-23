87d63c4da1ce72b92fc722b1a83d2eb35b28f161
SushilCaretor <k.sushil@careator.com>
2021-12-23 23:26:29 +0530

    last

--- dist/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { TwilioClient } from "nestjs-twilio";
-export declare class AppService {
-    private readonly client;
-    constructor(client: TwilioClient);
-    getHello(): string;
-    sendWhatsApp(data: any): Promise<any>;
-}
